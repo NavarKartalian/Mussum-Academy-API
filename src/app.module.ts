4b8533a52148766662a96ecdbeb65745ccc0754b
Navar Kartalian <alpha.hunter88@gmail.com>
2022-10-23 15:01:18 -0300

    :construction: setup completed

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppService } from './app.service';
+import { StudentsModule } from './modules/students/students.module';
+ import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    ConfigModule.forRoot(),
+    StudentsModule
+  ],
+  providers: [AppService],
+})
+export class AppModule {}
